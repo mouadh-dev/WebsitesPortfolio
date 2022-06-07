d320b72c74a192c6fbc4659e008fcbdd73a879ca
mouadh-dev <mouadh.laa@gmail.com>
2022-06-07 11:20:26 +0100

    edit menu

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -13,7 +13,7 @@
             <a class="nav-link" href="/">HOME <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/about">ABOUT ME</a>
+            <a class="nav-link" href="/about">ABOUT US</a>
           </li>
           {{-- <li class="nav-item">
             <a class="nav-link" href="portfolio.html">PORTFOLIO</a>
